43400ca479a7468dc4c50ace3954ef977d6355a4
Samuel Grossman <samuelgr@users.noreply.github.com>
2024-12-06 09:55:27 -0800

    Improve organization of test case definition macros to support use inside of arbitrary namespaces.

--- Include/Infra/Test/TestCase.h
+++ Include/Infra/Test/TestCase.h
@@ -57,16 +57,26 @@
 /// skipped. Automatically instantiates the proper test case object and registers it with the
 /// harness. Treat this macro as a function declaration; the test case is the function body.
 #define TEST_CASE_CONDITIONAL(name, cond)                                                          \
+  class TestCase_##name : public ::Infra::Test::ITestCase                                          \
+  {                                                                                                \
+  public:                                                                                          \
+                                                                                                   \
+    inline TestCase_##name(void) : ::Infra::Test::ITestCase(_CRT_WIDE(#name)) {}                   \
+    bool CanRun(void) const override;                                                              \
+    void Run(void) const override;                                                                 \
+  };                                                                                               \
+                                                                                                   \
   namespace _Infra_TestCaseInternal                                                                \
   {                                                                                                \
-    inline constexpr wchar_t kTestName__##name[] = L#name;                                         \
-    ::Infra::Test::TestCase<kTestName__##name> testCaseInstance__##name;                           \
+    static TestCase_##name testCaseInstance_##name;                                                \
   }                                                                                                \
-  bool ::Infra::Test::TestCase<_Infra_TestCaseInternal::kTestName__##name>::CanRun(void) const     \
+                                                                                                   \
+  bool TestCase_##name::CanRun(void) const                                                         \
   {                                                                                                \
     return (cond);                                                                                 \
   }                                                                                                \
-  void ::Infra::Test::TestCase<_Infra_TestCaseInternal::kTestName__##name>::Run(void) const
+                                                                                                   \
+  void TestCase_##name::Run(void) const
 
 /// Recommended way of creating test cases that execute unconditionally.
 /// Just provide the test case name.
@@ -89,33 +99,12 @@ namespace Infra
 
       /// Performs run-time checks to determine if the test case represented by this object can be
       /// run. If not, it will be skipped.
+      /// @return `true` if the test case represented by this object can be run, `false` otherwise.
       virtual bool CanRun(void) const = 0;
 
       /// Runs the test case represented by this object.
       /// Implementations are generated when test cases are created using the #TEST_CASE macro.
       virtual void Run(void) const = 0;
-    };
-
-    /// Concrete test case object template.
-    /// Each test case created by #TEST_CASE instantiates an object of this type with a different
-    /// template parameter.
-    /// @tparam kName Name of the test case.
-    template <const wchar_t* kName> class TestCase : public ITestCase
-    {
-    public:
-
-      inline TestCase(void) : ITestCase(kName) {}
-
-      /// Obtains the name of this test case, which was passed into the `TEST_CASE` macro.
-      /// @return Test case name. Guaranteed to be null-terminated.
-      inline std::wstring_view TestCaseName(void) const
-      {
-        return std::wstring_view(kName);
-      }
-
-      // ITestCase
-      bool CanRun(void) const override;
-      void Run(void) const override;
     };
 
     /// Thrown to signal a test failure. For internal use only.
