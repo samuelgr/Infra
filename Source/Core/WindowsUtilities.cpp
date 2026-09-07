d521653d96e6720ef8ebe19e9477b260b6f8c567
Samuel Grossman <samuelgr@users.noreply.github.com>
2026-09-07 09:00:19 -0700

    Add support for ordinal numbers to SafeGetProcAddress.

--- Source/Core/WindowsUtilities.cpp
+++ Source/Core/WindowsUtilities.cpp
@@ -11,10 +11,12 @@
 
 #include "Core/WindowsUtilities.h"
 
+#include <optional>
 #include <string_view>
 
 #include "ApiWindows.h"
 
+#include "Core/Strings.h"
 #include "Core/TemporaryBuffer.h"
 
 namespace Infra
@@ -59,57 +61,105 @@ namespace Infra
 
       const DWORD exportDirectoryStartRva =
           ntHeaders->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_EXPORT].VirtualAddress;
+      if (0 == exportDirectoryStartRva) return nullptr;
+
       const DWORD exportDirectoryEndRva = exportDirectoryStartRva +
           ntHeaders->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_EXPORT].Size;
+
       const IMAGE_EXPORT_DIRECTORY* exportDirectory =
           reinterpret_cast<IMAGE_EXPORT_DIRECTORY*>(moduleBaseAddress + exportDirectoryStartRva);
 
       const DWORD* exportedFunctions =
           reinterpret_cast<const DWORD*>(moduleBaseAddress + exportDirectory->AddressOfFunctions);
-      const DWORD* exportedNames =
-          reinterpret_cast<const DWORD*>(moduleBaseAddress + exportDirectory->AddressOfNames);
-      const WORD* exportedNameOrdinals =
-          reinterpret_cast<const WORD*>(moduleBaseAddress + exportDirectory->AddressOfNameOrdinals);
 
-      for (DWORD exportIndex = 0; exportIndex < exportDirectory->NumberOfNames; ++exportIndex)
+      std::optional<DWORD> foundFunctionIndex = std::nullopt;
+
+      if (reinterpret_cast<size_t>(procName) <= 0x0FFFF)
       {
-        const std::string_view exportName =
-            reinterpret_cast<const char*>(moduleBaseAddress + exportedNames[exportIndex]);
-        if (procName != exportName) continue;
+        // Importing by ordinal.
 
-        const DWORD exportOrdinal = exportedNameOrdinals[exportIndex];
-        const DWORD exportFinalRva = exportedFunctions[exportOrdinal];
-        const size_t exportFinalAddress = moduleBaseAddress + exportFinalRva;
+        const DWORD requestedOrdinal = static_cast<DWORD>(reinterpret_cast<uintptr_t>(procName));
 
-        const bool isInExportDirectory =
-            ((exportFinalRva >= exportDirectoryStartRva) &&
-             (exportFinalRva < exportDirectoryEndRva));
-        if (false == isInExportDirectory)
+        if (requestedOrdinal < exportDirectory->Base ||
+            requestedOrdinal >= exportDirectory->Base + exportDirectory->NumberOfFunctions)
+          return nullptr;
+
+        foundFunctionIndex = requestedOrdinal - exportDirectory->Base;
+      }
+      else
+      {
+        // Importing by name.
+
+        const DWORD* exportedNames =
+            reinterpret_cast<const DWORD*>(moduleBaseAddress + exportDirectory->AddressOfNames);
+        const WORD* exportedNameOrdinals = reinterpret_cast<const WORD*>(
+            moduleBaseAddress + exportDirectory->AddressOfNameOrdinals);
+
+        for (DWORD i = 0; i < exportDirectory->NumberOfNames; ++i)
         {
-          return reinterpret_cast<FARPROC>(exportFinalAddress);
-        }
-        else
-        {
-          const std::string_view exportForwardString =
-              reinterpret_cast<const char*>(exportFinalAddress);
-          const size_t exportStringSeparatorPos = exportForwardString.find_last_of('.');
-          if (std::string_view::npos == exportStringSeparatorPos) return nullptr;
-
-          TemporaryVector<char> exportForwardModuleNameBuf;
-          for (size_t i = 0; i < exportStringSeparatorPos; ++i)
-            exportForwardModuleNameBuf.PushBack(exportForwardString[i]);
-          exportForwardModuleNameBuf.PushBack('\0');
-
-          const char* exportForwardModuleName = exportForwardModuleNameBuf.Data();
-          const char* exportForwardProcName =
-              exportForwardString.data() + (1 + exportStringSeparatorPos);
-
-          return SafeGetProcAddress(
-              GetModuleHandleA(exportForwardModuleName), exportForwardProcName);
+          const char* currentName =
+              reinterpret_cast<const char*>(moduleBaseAddress + exportedNames[i]);
+          if (0 == std::strcmp(procName, currentName))
+          {
+            foundFunctionIndex = exportedNameOrdinals[i];
+            break;
+          }
         }
       }
 
-      return nullptr;
+      if (false == foundFunctionIndex.has_value()) return nullptr;
+
+      const DWORD exportFinalRva = exportedFunctions[*foundFunctionIndex];
+      if (0 == exportFinalRva) return nullptr;
+
+      const size_t exportFinalAddress = moduleBaseAddress + exportFinalRva;
+      const bool isInExportDirectory =
+          ((exportFinalRva >= exportDirectoryStartRva) && (exportFinalRva < exportDirectoryEndRva));
+
+      if (true == isInExportDirectory)
+      {
+        // Export is forwarded.
+
+        const std::string_view exportForwardString =
+            reinterpret_cast<const char*>(exportFinalAddress);
+        const size_t exportStringSeparatorPos = exportForwardString.find_last_of('.');
+        if (std::string_view::npos == exportStringSeparatorPos) return nullptr;
+
+        TemporaryVector<char> exportForwardModuleNameBuf;
+        for (size_t i = 0; i < exportStringSeparatorPos; ++i)
+          exportForwardModuleNameBuf.PushBack(exportForwardString[i]);
+        exportForwardModuleNameBuf.PushBack('\0');
+
+        const char* exportForwardModuleName = exportForwardModuleNameBuf.Data();
+        const char* exportForwardProcName =
+            exportForwardString.data() + (1 + exportStringSeparatorPos);
+
+        if ('#' == exportForwardProcName[0])
+        {
+          // Forwarding is by ordinal number, rather than by name.
+
+          size_t exportForwardOrdinal =
+              static_cast<size_t>(std::strtoul(&exportForwardProcName[1], nullptr, 10));
+          if (exportForwardOrdinal > 0x0FFFF) return nullptr;
+          exportForwardProcName = reinterpret_cast<LPCSTR>(exportForwardOrdinal);
+        }
+
+        auto exportForwardToModule = GetModuleHandleA(exportForwardModuleName);
+        if (nullptr == exportForwardToModule)
+        {
+          exportForwardModuleNameBuf.PopBack();
+          exportForwardModuleNameBuf.PushBack('.');
+          exportForwardModuleNameBuf.PushBack('d');
+          exportForwardModuleNameBuf.PushBack('l');
+          exportForwardModuleNameBuf.PushBack('l');
+          exportForwardModuleNameBuf.PushBack('\0');
+          exportForwardToModule = GetModuleHandleA(exportForwardModuleName);
+        }
+
+        return SafeGetProcAddress(exportForwardToModule, exportForwardProcName);
+      }
+
+      return reinterpret_cast<FARPROC>(exportFinalAddress);
     }
   } // namespace Windows
 } // namespace Infra
